0d66c5cd5bfd75566eaf528dcb6aadbacabb4f8e
rroycho <ritwikaroyc@gmail.com>
2015-04-06 19:20:32 -0400

    commitin server n client

--- Project1/src/Client.java
+++ Project1/src/Client.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 public class Client extends Thread{
 	Socket clientSocket = null;
 	static final int portNum = 7734;
-	
+
 	public static Command parseCommand(String command){
 		String[] cmdArray = command.split(" ");
 		if(cmdArray[0].equalsIgnoreCase("add"))
@@ -20,24 +20,47 @@ public class Client extends Thread{
 			return Command.LOOKUP;
 		return Command.WRONG;
 	}
-	
+
 	public static void main(String[] args) {
 		try{
 			Socket client = new Socket("localhost", portNum);
 			System.out.println("Enter the command:");
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			String command = br.readLine();
-			Command cmd = parseCommand(command);
-			switch(cmd){
-			case ADD:
-				break;
-				
-			}
 			OutputStream outToServer = client.getOutputStream();
-			DataOutputStream out = new DataOutputStream(outToServer);
-			out.writeUTF(command);
-			
-			client.close();
+			DataOutputStream out  = new DataOutputStream(outToServer);
+			while(true){
+				String command = br.readLine();
+				Command cmd = Command.WRONG;
+				String[] cmdArray = command.split(" ");
+
+				if(cmdArray[0].equalsIgnoreCase("add"))
+					cmd =  Command.ADD;
+				if(cmdArray[0].equalsIgnoreCase("list"))
+					cmd = Command.LIST;
+				if(cmdArray[0].equalsIgnoreCase("lookup"))
+					cmd = Command.LOOKUP;
+				if(cmd == Command.WRONG){
+					continue;
+				}else{
+					out.writeUTF(command);
+					System.out.print("Host : ");
+					String hostname = br.readLine();
+					System.out.println();
+					out.writeUTF("Host:"+ hostname);
+					System.out.print("Port : ");
+					int portNum = Integer.parseInt(br.readLine());
+					System.out.println();
+					out.writeUTF("Port:"+ portNum);
+					if(cmd == Command.ADD || cmd == Command.LOOKUP){
+						System.out.print("Title : ");
+						String title = br.readLine();
+						System.out.println();
+						out.writeUTF("Title:"+ title);
+					}
+				}
+				//TODO :: server response part 
+				client.close();
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
--- Project1/src/Server.java
+++ Project1/src/Server.java
@@ -1,11 +1,17 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.SocketException;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+
 class RFCData{
 	String title;
 	List<String> peers;
@@ -13,10 +19,11 @@ class RFCData{
 public class Server extends Thread{
 	static final int portNum = 7734;
 	ServerSocket myService = null ;
-	Set<String> activePeers;
+	Map<String, Integer> activePeers;
 	Map<Integer, RFCData> peerMap;
 	public Server(){
-		
+		activePeers = new HashMap<String, Integer>();
+		peerMap = new HashMap<Integer, RFCData>();
 		try {
 			myService = new ServerSocket(portNum);
 			myService.setSoTimeout(10000);
@@ -26,22 +33,94 @@ public class Server extends Thread{
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run(){
 		while(true){
 			try {
 				Socket serverSocket = myService.accept();
-				//serverSocket.getRemoteSocketAddress()
+				DataInputStream in = new DataInputStream(serverSocket.getInputStream());
+				OutputStream outToServer = serverSocket.getOutputStream();
+				DataOutputStream out  = new DataOutputStream(outToServer);
+
+				String command[] = in.readUTF().split(" ");
+				Command cmd = Command.WRONG;
+				if(command[0].equalsIgnoreCase("add"))
+					cmd =  Command.ADD;
+				if(command[0].equalsIgnoreCase("list"))
+					cmd = Command.LIST;
+				if(command[0].equalsIgnoreCase("lookup"))
+					cmd = Command.LOOKUP;
+
+				String hostname, title;
+				int portNumber, RFCNum;
+				RFCData data;
+				switch(cmd){
+				case WRONG:
+					break;
+				case ADD:
+					hostname = in.readUTF();
+					portNumber = Integer.parseInt(in.readUTF());
+					activePeers.put(hostname, portNumber);
+					RFCNum = Integer.parseInt(command[2]);
+					title = in.readUTF();
+
+					if(peerMap.containsKey(RFCNum)){
+						data = peerMap.get(RFCNum);
+						data.peers.add(hostname);
+					}else{
+						data = new RFCData();
+						data.peers = new ArrayList<String>();
+						data.peers.add(hostname);
+						data.title = title;
+					}
+					
+					//TODO :: output pending
+					out.writeUTF("RFC " + RFCNum + " " + title + " " + hostname + " " + portNumber);
+					
+					break;
+				case LOOKUP:
+					hostname = in.readUTF();
+					portNumber = Integer.parseInt(in.readUTF());
+					RFCNum = Integer.parseInt(command[2]);
+					title = in.readUTF();
+
+					if(peerMap.containsKey(RFCNum)){
+						data = peerMap.get(RFCNum);
+						List<String> peerlist = data.peers;
+						for(String peer : peerlist){
+							portNumber = activePeers.get(peer);
+							out.writeUTF("RFC " + RFCNum + " " + title + " " + peer + " " + portNumber);
+						}
+					}else{
+						out.writeUTF("Sorry! Currently none of the active peers have RFC "+ RFCNum);
+					}
+					break;
+				case LIST:
+					Iterator<Map.Entry<Integer, RFCData>> it = peerMap.entrySet().iterator();
+					while(it.hasNext()){
+						Map.Entry<Integer, RFCData> entry = it.next();
+						RFCNum = entry.getKey();
+						data = entry.getValue();
+						List<String> peerlist = data.peers;
+						for(String peer : peerlist){
+							portNumber = activePeers.get(peer);
+							out.writeUTF("RFC " + RFCNum + " " + data.title + " " + peer + " " + portNumber);
+						}
+					}
+					break;
+					//TODO :: case close:
+				}
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	public static void main(String args[]){
-		
+
 
 		new Server().start();
-		
+
 	}
 }
